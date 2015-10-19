09956548e01b9bd8e67f4ddab4030da5a455985b
Dorian Gaillot <gaillotdorian@gmail.com>
2015-10-19 17:13:22 +0200

    changing js bahaviour on welcome page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -20,17 +20,19 @@ $(document).
         console.log(element.currentTarget);
         var theTarget=$(element.currentTarget);
         var nameToReach=theTarget.data('content');
+        var explanationToReach=theTarget.data('explanation');
         var customColor='#'+ theTarget.data('customcolor');
         nameToReach='<p style="color:'+customColor+';">'+nameToReach+'</p>';
         $('.homeDestination').html(nameToReach);
+        $('.homeSectionDescription').html(explanationToReach);
 
 
 
     }
 
     function handlerOut(element){
-        console.log(element);
-        console.log(element.currentTarget);
+        //console.log(element);
+        //console.log(element.currentTarget);
     }
 
 
