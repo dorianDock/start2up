a34eb8ad4cf269bd5ce6c1f5b2d88778be384938
Dorian Gaillot <gaillotdorian@gmail.com>
2015-11-08 23:34:12 +0100

    changing completely the homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,28 +12,28 @@ $(document).
         })
     ;
 
-    function handlerIn(element){
-        console.log(element);
-        console.log(element.currentTarget);
-        var theTarget=$(element.currentTarget);
-        var nameToReach=theTarget.data('content');
-        var explanationToReach=theTarget.data('explanation');
-        var customColor='#'+ theTarget.data('customcolor');
-        nameToReach='<p style="color:'+customColor+';">'+nameToReach+'</p>';
-        $('.homeDestination').html(nameToReach);
-        $('.homeSectionDescription').html(explanationToReach);
-
-
-
-    }
-
-    function handlerOut(element){
-        //console.log(element);
-        //console.log(element.currentTarget);
-    }
-
-
-    $('.homeSquare').hover(handlerIn, handlerOut);
+    //function handlerIn(element){
+    //    console.log(element);
+    //    console.log(element.currentTarget);
+    //    var theTarget=$(element.currentTarget);
+    //    var nameToReach=theTarget.data('content');
+    //    var explanationToReach=theTarget.data('explanation');
+    //    var customColor='#'+ theTarget.data('customcolor');
+    //    nameToReach='<p style="color:'+customColor+';">'+nameToReach+'</p>';
+    //    $('.homeDestination').html(nameToReach);
+    //    $('.homeSectionDescription').html(explanationToReach);
+    //
+    //
+    //
+    //}
+    //
+    //function handlerOut(element){
+    //    //console.log(element);
+    //    //console.log(element.currentTarget);
+    //}
+    //
+    //
+    //$('.homeSquare').hover(handlerIn, handlerOut);
 
 
 
