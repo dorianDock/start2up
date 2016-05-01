bfe06c53f02cd34dbc91198ef3463658b380d068
Dorian <gaillotdorian@gmail.com>
2016-05-02 00:47:46 +0200

    style is nearly ok for comments > need come more js + link a user to a comment

--- app/assets/javascripts/useful_links.js
+++ app/assets/javascripts/useful_links.js
@@ -2,17 +2,20 @@
 $(document).
     on('page:change', function () {
         if($('.usefulLinksCategory').length!==0){
-            InitializeSelectList('usefulLinksCategory', 'Choisir une catégorie');
+                InitializeSelectList('usefulLinksCategory', 'Choisir une catégorie');
         }
         if($('.usefulLinksCategoryEdit').length!==0){
-        $('.usefulLinksCategoryEdit').each(function(index, element){
-            var elementId=$(element).data('objectlinkid');
-            var nameOfTheElementClass="usefulLinksCategory"+elementId;
-            InitializeSelectList(nameOfTheElementClass, 'Choisir une catégorie');
+            $('.usefulLinksCategoryEdit').each(function(index, element){
+                var elementId=$(element).data('objectlinkid');
+                var nameOfTheElementClass="usefulLinksCategory"+elementId;
+                InitializeSelectList(nameOfTheElementClass, 'Choisir une catégorie');
+            });
+        }
+
+        $('.formForComment').toggle();
+        $('.replyToComment').click(function(){
+           var commentToReplyTo=$(this).data('commentid');
+            $('.formForComment'+commentToReplyTo).toggle();
         });
-
-
-    }
-
 
     });
