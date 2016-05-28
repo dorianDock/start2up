2cb9e9f5b870a3d1cdcced9aab892078b7ef4a18
Dorian <gaillotdorian@gmail.com>
2016-05-28 12:15:06 +0200

    making usefullinks use home views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,63 +12,54 @@ class HomeController < ApplicationController
     @usefulLinks=UsefulLink.reverse_order.public_links.all.ideas
     @page_title='Idées'
     build_empty_comment
-    render "useful_links/link_list"
   end
 
   def find_your_soul
     @usefulLinks=UsefulLink.reverse_order.public_links.all.associates
     @page_title="S'associer"
     build_empty_comment
-    render "useful_links/link_list"
   end
 
   def law_is_law
     @usefulLinks=UsefulLink.reverse_order.public_links.all.law
-    @page_title="Le légal"
+    @page_title='Le légal'
     build_empty_comment
-    render "useful_links/link_list"
   end
 
   def money_broke
     @usefulLinks=UsefulLink.reverse_order.public_links.all.money
     @page_title='Le financement'
     build_empty_comment
-    render "useful_links/link_list"
   end
 
   def useful_information
     @usefulLinks=UsefulLink.reverse_order.public_links.all.info
     @page_title='Informations Générales'
     build_empty_comment
-    render "useful_links/link_list"
   end
 
   def the_godfather
     @usefulLinks=UsefulLink.reverse_order.public_links.all.mentorship
     @page_title='Les associés'
     build_empty_comment
-    render "useful_links/link_list"
   end
 
   def find_your_roof
     @usefulLinks=UsefulLink.reverse_order.public_links.all.housing
     @page_title='Les bureaux'
     build_empty_comment
-    render "useful_links/link_list"
   end
 
   def low_cost_consulting
     @usefulLinks=UsefulLink.reverse_order.public_links.all.info
     @page_title='un Test'
     build_empty_comment
-    render 'useful_links/link_list'
   end
 
   def technical_links
     @usefulLinks=UsefulLink.reverse_order.public_links.all.technical
     @page_title='Liens Techniques'
     build_empty_comment
-    render 'useful_links/link_list'
   end
 
 
