05a41d577f369eaf80f2f15bcac8126794349709
Dorian <gaillotdorian@gmail.com>
2016-05-29 18:39:23 +0200

    MAking welcome page cuter and adding more interaction through mouse movements detection

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,46 @@
 //encoding: utf-8
 
+function CalculateDistance(firstX, secondX, firstY, secondY){
+    var distanceToBlock=Math.sqrt(Math.pow((firstX-secondX),2)+Math.pow((firstY-secondY),2));
+    return distanceToBlock;
+}
+
+
 $(document).
     on('page:change', function () {
 
-    $('.homeSquareMenu')
+    $('.homeSquare')
         .popup({
             inline   : true,
-            hoverable: false,
-            position : 'right center',
+            hoverable: true,
 
         })
     ;
+
+    $('body').on('mousemove',function(event){
+        var x = event.clientX;
+        var y = event.clientY;
+
+        $('.homeSquare').each(function() {
+            var myElement= $(this);
+            var xContainer=myElement.offset().left;
+            xContainer=xContainer+myElement.width()/2;
+            var yContainer=myElement.offset().top;
+            yContainer=yContainer+myElement.height()/2;
+            var distanceToBlock=CalculateDistance(xContainer,x,yContainer,y);
+            if(distanceToBlock<=400){
+                myElement
+                    .popup('show')
+                ;
+            } else{
+                myElement
+                    .popup('hide')
+                ;
+            }
+        });
+    });
+
+
 
     //function handlerIn(element){
     //    console.log(element);
